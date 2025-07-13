714827bdfa5ada1b137cd7afaa34579a59a927ad
Jardel <jazanolla@gmail.com>
2025-07-13 15:38:26 -0300

    Test

--- scripts/pack-report.ts
+++ scripts/pack-report.ts
@@ -2,9 +2,6 @@ import fs from 'fs';
 import path from 'path';
 import { inlineSource } from 'inline-source';
 
-/**
- * Caminho para o diretório do relatório gerado pelo @wdio/visual-reporter
- */
 const reportPath = path.resolve(__dirname, '../visual-report/report');
 const inputFile = path.join(reportPath, 'index.html');
 const outputFile = path.join(reportPath, 'index-packed.html');
